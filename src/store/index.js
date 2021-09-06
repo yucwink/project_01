76b9a760d0c9bc3960920221cb63f269825b1d44
yuc <1120062005@qq.com>
2021-09-06 14:18:56 +0800

    todolist

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,80 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 所有的任务列表
+    list: [],
+    inputValue: 'aaa',
+    nextId: 5,
+    viewKey: 'all'
   },
   mutations: {
+    // 创建mutation未state赋值
+    initList(state, list) {
+      this.state.list = list
+    },
+    addlist(state) {
+      const obj = {
+        id: state.nextId++,
+        info: state.inputValue,
+        done: false
+      }
+      state.list.push(obj)
+      state.inputValue = ''
+    },
+    // 根据id删除对应的任务事项
+    removeList(state, id) {
+      // 根据id查找对应项的所有
+      const i = state.list.findIndex(x => x.id === id)
+      // 根据索引
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    changeStatus(state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    // 清除已完成任务
+    cleanDone(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    changeViewKey(state, key) {
+      state.viewKey = key
+    },
+    setInputValue(state, val) {
+      state.inputValue = val
+    }
   },
   actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        context.commit('initList', data)
+      })
+    }
+  },
+  getters: {
+    // 统计未完成任务条数
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infolist(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'undone') {
+        return state.list.filter(x => !x.done)
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter(x => x.done)
+      }
+    }
   },
   modules: {
   }
